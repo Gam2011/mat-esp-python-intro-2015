8f2948a8f39d3d97cccddb4e3128e0f77cb8f9a6
Marcela Claudio <marcelarclaudio@hotmail.com>
2015-09-18 10:40:49 -0300

    Estou adicionando ao arquivo uma linha que mostra a lista original

--- bubble.py
+++ bubble.py
@@ -1,4 +1,6 @@
 bubble = [11,18,3,1,16,12,6,19,5,0,14,4,17,9,13,7,10,15,2,8] #aqui criamos a lista crua e fora de ordem.
+print("Lista original: "bubble) # imprimindo a lista original fora de ordem
+
 a = 20 #aqui criamos uma variável com o número de itens da lista.
 copo = 0 #aqui criamos a variável copo, que utilizaremos para realizar as trocas de itens.
 count = 300 #aqui criamos uma contagem bem grande, pra garantir que o programa fará o trabalho completo
@@ -12,4 +14,4 @@ while count > 0: #enquanto a variável contagem não se esgotar, o programa roda
 			bubble[i+1] = copo #o copo vira o item comparado
 #efetivamente, o que fizemos foi trocar um item pelo outro.
 
-print(bubble) #após ele repetir o loop 300 vezes imprimimos, por fim, o resultado.
+print("Lista em ordem crescente: "bubble) #após ele repetir o loop 300 vezes imprimimos, por fim, o resultado.
